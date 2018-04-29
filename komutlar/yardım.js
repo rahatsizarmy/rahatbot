1915f3e4b470c71358254b0e0545cd93f9d9ed7c
doganmu <38323436+doganmu@users.noreply.github.com>
2018-04-29 21:02:59 +0300

    Add files via upload

--- komutlar/yardım.js
+++ komutlar/yardım.js
@@ -9,7 +9,7 @@ exports.run = (client, message, params) => {
   .setAuthor(message.author.username, message.author.avatarURL)
   .setTitle('Komut listesi')
   .addField('Genel Komutlar','rb!ping == pingimi gösterir.\nrb!sunucubilgi == sunucu hakkında blgi verir.\nrb!yardım  ==  Botun Komutlarını Gösterir.\nrb!avatar ==  Etiketledğiniz Kişinin Avatarını Verir.\nrb!bilgi  == Bot Hakkında Bilgi Verir.')
-  .addField('Moderasyon','rb!ban == birini suncudan yasaklar\nrb!kick == birini sunucudan atar.\nrb!temizle == Sohbeti Temizler\nrb!uyar == Bir Kişiyi Uyarırsınız\nrb!duyuru == Güzel Bir Duyuru Görünümü Sağlar')
+  .addField('Moderasyon','rb!ban == birini suncudan yasaklar\nrb!kick == birini sunucudan atar.\nrb!temizle == Sohbeti Temizler\nrb!uyar == Bir Kişiyi Uyarırsınız\nrb!duyur == Güzel Bir Duyuru Görünümü Sağlar')
   return message.channel.sendEmbed(embed)
 };
 
