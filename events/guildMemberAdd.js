29f8fc918f19ce4cd24e7dac262709d28e378de4
dfatihdoganmu <38323436+dfatihdoganmu@users.noreply.github.com>
2018-05-04 16:01:23 +0300

    Add files via upload

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,5 +1,5 @@
 module.exports = member => {
-  const channel = member.guild.channels.find('name', 'sohbet');
+  const channel = member.guild.channels.find('name', 'rgelen-giden');
   if (!channel) return;
   channel.send(`**${member}** **Sunucuya Hosgeldin Keyfine Bak :smile:!**`);
 };
